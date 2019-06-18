3b1c17e6e8e1504f5572c904bde7945413500211
Tom Vajtay <vajtayt@gmail.com>
2019-06-18 16:41:27 -0400

    Adding comments and license name

--- 2pMovementInterpolation.py
+++ 2pMovementInterpolation.py
@@ -14,12 +14,14 @@ import numpy as np
 
 #Load in Data, this may need to be changed but I saved the binary index of movement and the concatenated TIFF in an HDF5 file.
 Data = h5py.File('2D1_Day1.hdf5','r')
+#Need to work on the previous step of how to ID bad frames, also maybe a reference frame approach?
 
-#Seperate data
+#Seperate data and change to numpy arrays
 index = Data['/index'][()] #New way to do .value
 stack = Data['/stack'][()]
 
-#Michael Osthege vectorized 1-D interpolator
+#Michael Osthege's vectorized 1-D interpolator @michaelosthege
+#https://gist.github.com/michaelosthege/e20d242bc62a434843b586c78ebce6cc
 def interpolate(x_fix, y_fix, x_var):
     x_repeat = np.tile(x_var[:,None], (len(x_fix),))
     distances = np.abs(x_repeat - x_fix)
@@ -37,6 +39,12 @@ def interpolate(x_fix, y_fix, x_var):
 
 
 def badframe_interpolation(index, stack):
+    """
+    Cycles through a supplied index list and 1D interpolates between the frames
+    before and after the indexed movement/bad frames in the stack numpy array
+    'index' is a 1D numpy array
+    'stack' is a 3D numpy array with indices [frames, height, width]
+    """
     flag = 1
     i = 0
     max_frames = len(index)
@@ -64,7 +72,7 @@ def badframe_interpolation(index, stack):
                 while index[i] and i < max_frames:
                     i = i + 1
 
-                if i == max_frames:
+                if i == max_frames: #Worst case scenario, movement till the end
                     for j in range(i,max_frames):
                         stack[j,:,:] = end_source
                     flag = 0
@@ -75,6 +83,7 @@ def badframe_interpolation(index, stack):
                 # Create sequential list of frames to interpolate
                 frames = range(start_frame,i)
 
+                #Need to watch out for how the dimensions maintain orientation
                 stack[start_frame:i,:,:] = interpolate(frames,[start_frame, i],[start_source, end_source])
 
             else:
@@ -84,3 +93,4 @@ def badframe_interpolation(index, stack):
                 else:
                     flag = 0
                     break
+    return stack
